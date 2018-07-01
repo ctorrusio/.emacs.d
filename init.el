3228da6e17733cd5a604acf8ef16ef0ce32d6c5d
Cristian Torrusio <cristian.torrusio@gmail.com>
2018-07-02 01:56:54 +0200

    added custom key bindings as a minor mode, added ido.last to .gitignore

--- init.el
+++ init.el
@@ -42,8 +42,37 @@
  )
 
 
+
+
 ;; enabled minor modes
 (ido-mode t)
 
 
+
+
+;; own keybindings in a minor mode to override all other bindings globally.
+
+(defvar my-keys-minor-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-z") 'undo)
+    map)
+  "my-keys-minor-mode keymap.")
+
+(define-minor-mode my-keys-minor-mode
+  "A minor mode so that my key settings override annoying major modes."
+  :init-value t
+  :lighter " my-keys")
+(defun my-minibuffer-setup-hook ()
+  (my-keys-minor-mode 0))
+(defun my-keys-have-priority (_file)
+  "Try to ensure that my keybindings retain priority over other minor modes.
+Called via the `after-load-functions' special hook."
+  (unless (eq (caar minor-mode-map-alist) 'my-keys-minor-mode)
+    (let ((mykeys (assq 'my-keys-minor-mode minor-mode-map-alist)))
+      (assq-delete-all 'my-keys-minor-mode minor-mode-map-alist)
+      (add-to-list 'minor-mode-map-alist mykeys))))
+
+(my-keys-minor-mode 1)
+(add-hook 'minibuffer-setup-hook 'my-minibuffer-setup-hook)
+(add-hook 'after-load-functions 'my-keys-have-priority)
 
